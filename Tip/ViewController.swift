b4fc1a839b956f801d16f2c9f2a1a3579990dc6b
Parsha <mantaqaakhan@mantaqaas-macbook-pro.local>
2018-12-27 16:39:35 -0500

    Initial Commit

---
+++ Tip/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tip
+//
+//  Created by Mantaqaa Khan on 12/27/18.
+//  Copyright © 2018 lanka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
